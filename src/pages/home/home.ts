3d22835ed432dbba5413f45d09d3c65b6f02b412
alexpro <tro_alex@mail.ru>
2017-11-12 15:49:25 +0300

    base layout of application

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,24 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // private abcSet : any = [
+  //     {'name': "a", 'img': 'logo'},
+  //     {'name': "b", 'img': 'logo'}
+  // ];
+  public currentLetter : string = 'a';
+  private abcSet : any = [
+    'a', 'b', 'c', 'd', 'e'
+    // 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'
+  ];
+
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  setCurrentLetter(event) {
+    this.currentLetter = event.target.alt;
+    // console.log(event.target.alt);
   }
 
 }
