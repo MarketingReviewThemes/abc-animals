7fe8594d6194e4be3fb68d417e4a2ad8c7913d80
alexpro <tro_alex@mail.ru>
2017-11-22 01:42:07 +0300

    added playing audio on click

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Media, MediaObject} from '@ionic-native/media';
+
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  // private abcSet : any = [
-  //     {'name': "a", 'img': 'logo'},
-  //     {'name': "b", 'img': 'logo'}
-  // ];
-  public currentLetter : string = 'a';
-  private abcSet : any = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-  ];
+    // private abcSet : any = [
+    //     {'name': "a", 'img': 'logo'},
+    //     {'name': "b", 'img': 'logo'}
+    // ];
+    public currentLetter: string = 'a';
+    private abcSet: any = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
 
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private media: Media) {
 
-  }
-
-  setCurrentLetter(event) {
-    if (event.target.alt) {
-      this.currentLetter = event.target.alt;
     }
 
-    //animate clicked button
-    //not working in ios
-    // event.target.animate([
-    //   { transform: 'scale(0.8)' },
-    //   { transform: 'scale(1)' }
-    // ], {
-    //   duration: 750,
-    //   easing: 'ease-in'
-    // });
-    // console.log(event.target.alt);
-  }
+
+    setCurrentLetter(event) {
+        if (event.target.alt) {
+            this.currentLetter = event.target.alt;
+            let mediaFile =  "assets/audio/" + this.currentLetter +".m4a";
+
+            const file: MediaObject = this.media.create(mediaFile);
+            file.play();
+        }
+
+        //animate clicked button
+        //not working in ios
+        // event.target.animate([
+        //   { transform: 'scale(0.8)' },
+        //   { transform: 'scale(1)' }
+        // ], {
+        //   duration: 750,
+        //   easing: 'ease-in'
+        // });
+        // console.log(event.target.alt);
+    }
 
 }
